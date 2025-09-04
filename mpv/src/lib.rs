6bd0d16eea9ed0393ebfc5f475f13b45b70f1291
julia <me@juliapaci.com>
2025-09-05 00:35:45 +1000

    simple abstraction
    elements as types that get converted into glsl through basic implementable blocks. more should be automated tbh so ill do that but also the main issue is making it dynamic, somehow we might have to hook mpv internals to give us some SSBO

--- mpv/src/lib.rs
+++ mpv/src/lib.rs
@@ -1,17 +1,23 @@
-use mpv_client::{mpv_handle, Event, Handle};
+use mpv_client::{Event, Handle, mpv_handle};
 mod shader;
 
-static NAME_PROP_SHADER: &str = "glsl-shaders";
+static PROP_SHADER: &str = "glsl-shaders";
 
 #[unsafe(no_mangle)]
 extern "C" fn mpv_open_cplugin(handle: *mut mpv_handle) -> std::os::raw::c_int {
+    println!("mappiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+
     let client = Handle::from_ptr(handle);
 
-    let mut shader = shader::Builder::new().unwrap();
-    shader.refresh().unwrap();
+    let mut shader = shader::Builder::new().unwrap()
+        .with_element(shader::Rectangle::new().pos(0.4, 0.4).size(0.2, 0.2))
+        .with_element(shader::Circle::new().pos(0.4+0.2, 0.4+0.2).rad(0.01))
+        .with_element(shader::Circle::new().pos(0.4-0.2, 0.4-0.2).rad(0.01))
+        .with_element(shader::Circle::new().pos(0.4+0.2, 0.4-0.2).rad(0.01))
+        .with_element(shader::Circle::new().pos(0.4-0.2, 0.4+0.2).rad(0.01));
+    shader.sync().unwrap();
+    _ = client.set_property(PROP_SHADER, shader.shaderpath().unwrap());
     loop {
-        _ = client.set_property(NAME_PROP_SHADER, shader.shader().unwrap());
-
         match client.wait_event(-1.) {
             Event::Shutdown => return 0,
             event => println!("Got event: {}", event),
--- mpv/src/shader.rs
+++ mpv/src/shader.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::{self, io::Write};
 use tempfile;
 
 macro_rules! stringStrings {
@@ -8,29 +8,37 @@ macro_rules! stringStrings {
     };
 }
 
-/// NOTE: x,y,w,h r normalised!
+trait Element {
+    // fn position(&self) -> (f32, f32);
+    // mayb setsize and stuff here
+    // pub fn add(&mut self, offset: (f32, f32)) -> Self;
+    fn opacity(&self) -> f32 { 1.0 }
+
+    fn sdf(&self) -> String;
+}
+
 pub struct Builder {
     shader: tempfile::NamedTempFile,
-    x: f32,
-    y: f32,
-    w: f32,
-    h: f32
+    elements: Vec<Box<dyn Element>>,
 }
 
 impl Builder {
     pub fn new() -> std::io::Result<Self> {
         Ok(Self {
             shader: tempfile::NamedTempFile::new()?,
-            x: 0.4,
-            y: 0.4,
-            w: 0.2,
-            h: 0.2,
+            elements: Vec::new(),
         })
     }
 
-    pub fn add(&mut self, offset: (f32, f32)) {
-        self.x += offset.0;
-        self.y += offset.1;
+    pub fn with_element<T: Element + 'static>(mut self, element: T) -> Self {
+        self.elements.push(Box::new(element));
+        self
+    }
+
+    pub fn sync(&mut self) -> std::io::Result<()> {
+        self.shader = tempfile::NamedTempFile::new()?;
+        let src = self.build();
+        write!(self.shader.as_file_mut(), "{src}")
     }
 
     fn build(&self) -> String {
@@ -39,14 +47,7 @@ impl Builder {
             // thanks Inigo Quilez
             //!HOOK MAIN
             //!BIND HOOKED
-            "#
 
-            format!(
-                "const vec4 REGION = vec4({}, {}, {}, {});",
-                self.x, self.y, self.w, self.h
-            )
-
-            r#"
             float sharpen(in float a) {
                 return smoothstep(0.01, 0.01+0.001, a);
             }
@@ -63,42 +64,125 @@ impl Builder {
             }
 
             vec4 hook() {
-                vec2 cpos = HOOKED_pos;
-                cpos.x *= HOOKED_size.x/HOOKED_size.y;
+                vec2 pos = HOOKED_pos;
+                pos.x *= HOOKED_size.x/HOOKED_size.y;
 
                 vec4 colour = HOOKED_tex(HOOKED_pos);
+            "#
 
-                const vec2 pos[4] = vec2[4](
-                    (REGION.xy - REGION.zw),
-                    (REGION.xy + REGION.zw),
-                    (REGION.xy - vec2(REGION.z, -REGION.w)),
-                    (REGION.xy + vec2(REGION.z, -REGION.w))
-                );
-                for(
-                    int i = 0;
-                    i < 4;
-                    i++
-                ) {
-                    float circle = sdCircle(pos[i] - cpos, 0.01);
-                    colour += (1 - circle)*0.5;
-                }
+            self.elements
+                .iter()
+                .map(|e| e.sdf())
+                .reduce(|a, e| a + "\n" + &e)
+                .unwrap_or_default()
 
-                float box = sdBox(REGION.xy - cpos, REGION.zw);
-                colour += (1 - box)*0.1;
+            r#"
+                // const vec2 pos[4] = vec2[4](
+                //     (REGION.xy - REGION.zw),
+                //     (REGION.xy + REGION.zw),
+                //     (REGION.xy - vec2(REGION.z, -REGION.w)),
+                //     (REGION.xy + vec2(REGION.z, -REGION.w))
+                // );
+                // for(
+                //     int i = 0;
+                //     i < 4;
+                //     i++
+                // ) {
+                //     float circle = sdCircle(pos[i] - cpos, 0.01);
+                //     colour += (1 - circle)*0.5;
+                // }
+                //
+                // float box = sdBox(REGION.xy - cpos, REGION.zw);
+                // colour += (1 - box)*0.1;
 
                 return colour;
             }
-        "#).to_owned()
+        "#)
+        .to_owned()
     }
 
-    pub fn refresh(&mut self) -> std::io::Result<()> {
-        self.shader = tempfile::NamedTempFile::new()?;
-        let src = self.build();
-        write!(self.shader.as_file_mut(), "{src}")?;
-        Ok(())
+    pub fn shaderpath(&self) -> std::io::Result<String> {
+        // hacky error conversion but o well
+        Ok(self
+            .shader
+            .path()
+            .to_str()
+            .ok_or(std::io::ErrorKind::Other)?
+            .to_owned())
+    }
+}
+
+/// NOTE: x,y,w,h r normalised!
+pub struct Rectangle {
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
+}
+
+pub struct Circle {
+    x: f32,
+    y: f32,
+    r: f32
+}
+
+impl Rectangle {
+    pub fn new() -> Self {
+        Self { x: 0., y: 0., w: 0., h: 0. }
     }
 
-    pub fn shader(&self) -> std::io::Result<String> {
-        Ok(self.shader.path().to_str().ok_or(std::io::ErrorKind::Other)?.to_owned())
+    pub fn pos(mut self, x: f32, y: f32) -> Self {
+        self.x = x;
+        self.y = y;
+        self
+    }
+
+    pub fn size(mut self, w: f32, h: f32) -> Self {
+        self.w = w;
+        self.h = h;
+        self
+    }
+}
+
+impl Circle {
+    pub fn new() -> Self {
+        Self { x: 0., y: 0., r: 0. }
+    }
+
+    pub fn pos(mut self, x: f32, y: f32) -> Self {
+        self.x = x;
+        self.y = y;
+        self
+    }
+
+    pub fn rad(mut self, r: f32) -> Self {
+        self.r = r;
+        self
+    }
+}
+
+impl Element for Rectangle {
+    fn opacity(&self) -> f32 {
+        0.1
+    }
+
+    fn sdf(&self) -> String {
+        format!(r#"{{
+            float box = sdBox(vec2({}, {}) - pos, vec2({}, {}));
+            colour += (1 - box)*0.1;
+        }}"#, self.x, self.y, self.w, self.h)
+    }
+}
+
+impl Element for Circle {
+    fn opacity(&self) -> f32 {
+        0.5
+    }
+
+    fn sdf(&self) -> String {
+        format!(r#"{{
+            float circle = sdCircle(vec2({}, {}) - pos, {});
+            colour += (1 - circle)*0.5;
+        }}"#, self.x, self.y, self.r)
     }
 }
